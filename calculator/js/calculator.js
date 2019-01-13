30ca05a126b30f208b90da465403edaaf49061c0
Brandon Li <brandon.li820@icloud.com>
2019-01-13 14:17:05 -0700

    merge conflicts

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,8 +1,7 @@
-/**
-  * main.js
+/*  js: module
+  * appendDiv.js
   * learning app
   * Created by Brandon Li on 1/11/19.
   * Copyright © 2019 Brandon Li. All rights reserved.
  */
-
- console.log("here")
+ console.log("calculate!")
