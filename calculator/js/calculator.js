46b149add6cbf16491ea0a0033bada5bc703acaa
Brandon Li <brandon.li820@icloud.com>
2019-01-16 14:59:35 -0700

    added initial js for ui in calculator

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -14,11 +14,34 @@
    solverTest.test(); 
  */
 
-  //uncomment to test
-   import SolverTest from "./../calculatorCore/solverTest.js";
-   var solverTest = new SolverTest();
-   solverTest.test(); 
  
 // import Solver from ".././calculatorCore/solver.js"
 // var solver = new Solver();
- 
+import Screen from "./../../dashboard/dashboardCore/screen.js";
+var screen = new Screen();
+
+//set up initial structure
+// screen.addDivToParentType( "div", "calculator_wrapper", "", "", "body" );
+
+// //add the return-to-dashboard button
+// addReturnButton( "./assets/reset.png", "../dashboard.html", "./assets/resetHover.png" )
+
+function addReturnButton( src, url , hoverSrc){
+    //add the app container
+    let button = screen.addChildToParentId( "div", "backToDashboardButton", "", "", "calculator_wrapper" );
+        
+    //add a button and set attributes
+    let image = screen.addDivToNode( "img", "", "backToDashboard", "", button );
+    image.setAttribute( "src", src );
+    //add functions
+    image.onmouseover = function() { 
+        image.setAttribute( "src", hoverSrc );
+    };
+    image.onmouseout = function() { 
+      image.setAttribute( "src", src );
+    };
+    image.onclick = function() { 
+      window.open(url, '_self');
+    };
+
+}
