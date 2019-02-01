4becab5c9c59d2432f39769e8795921d22ef8b01
Brandon Li <brandon.li820@icloud.com>
2019-01-31 19:13:44 -0700

    finished everyting but drag on slider

--- Slider/Slider.js
+++ Slider/Slider.js
@@ -8,6 +8,8 @@
  *
  * ## Functionality:
  *  - create the slider ui and link the css file
+ *  - Full List of All Parameters the user can modify (override) will be at the
+ *    bottom.
  */
 import ScreenView from "./../../ScreenView/ScreenView.js";
 var screen = new ScreenView();
@@ -19,8 +21,17 @@ export default class Slider{
    * @param optional {object} options - overide the defaults
    */  
   constructor( options ){
-    var sliderBackground = document.createElement( "div" );
     options = ( !options ) ? {} : options;
+    /**
+     * The Slider Background: the container containing the entire slider element
+     * with the value and the title.
+     */
+    var sliderLow =  ( options.low ) ? options.low : 0; 
+    var sliderTop =  ( options.Top ) ? options.Top : 1; 
+    var startingValue = ( options.startingValue ) ? 
+                          options.startingValue: "" + 0.5;
+    var round = ( options.round ) ? options.round: 2;
+    var sliderBackground = document.createElement( "div" );
     // after : is the default, the option object can overide
     // the background of the container of the slider itself
     var background = ( options.background ) ? options.background : "#FFF";
@@ -37,11 +48,11 @@ export default class Slider{
     // the box shadows of the background
     var boxShadow = ( options.boxShadow ) ? 
                       options.boxShadow : 
-                      "1px 1px 5px 1px #444";
+                      "1px 1px 3px 1px #999";
     sliderBackground.style.boxShadow = boxShadow;
 
     // the height of the background behind it.
-    var height = ( options.height ) ? options.height: "100px";
+    var height = ( options.height ) ? options.height: "auto";
     sliderBackground.style.height = height;
 
     // the width of the background behind it.
@@ -49,77 +60,159 @@ export default class Slider{
     sliderBackground.style.width = width;
 
     // the margin of the background behind it.
-    var margin = ( options.margin ) ? options.margin: "auto";
+    var margin = ( options.margin ) ? options.margin: "5% auto";
     sliderBackground.style.margin = margin;
 
-    // create the container of the value and the title
+    /**
+     * the container for the value and the title
+     */  
     var container = screen.createChild( "div", null, null, "" );
     container.style.display = "flex"
     container.style.justifyContent = "center"
-
+    // margin is 10%
+    container.style.margin = "1% auto";
+    container.style.alignItems = "center";
     // create the value 
-    var unit = ( options.unit ) ? options.unit: "m/s";
+    var unit = ( options.unit ) ? options.unit: "";
+  
+    var value = screen.createChild( "div", null, null, 
+                                      startingValue + " " + " " + unit );
     var valueBackground = ( options.valueBackground ) 
-                          ? options.valueBackground: "m/s";
-    var startingValue = ( options.startingValue ) ? options.startingValue: "0";
-    var value = screen.createChild( "div", null, null, startingValue + " " + " " + unit );
-    value.style.border = ( options.valueBorder ) ? options.valueBorder: "1px solid grey";
+                          ? options.valueBackground: "#EEE";
+    value.style.background = valueBackground;
+    value.style.border = ( options.valueBorder ) ? 
+                            options.valueBorder: "1px solid grey";
     value.style.textAlign = "center";
-    value.style.paddingTop = "0px"
-    value.style.fontSize = ( options.valueFontSize ) ? options.valueFontSize: "30px";
-    value.style.margin = "5px";
-    value.style.height = ( options.valueFontSize ) ? options.valueFontSize + 10: "40px";
+    value.style.fontSize = ( options.valueFontSize ) ? 
+                            options.valueFontSize: "20px";
+    var valuePaddingLeft = ( options.valuePaddingLeft ) ? 
+                            options.valuePaddingLeft: "30px";
+    value.style.paddingLeft = valuePaddingLeft;
+    var valuePaddingRight = ( options.valuePaddingRight ) ? 
+                            options.valuePaddingRight: "30px";
+    value.style.paddingRight = valuePaddingRight;
     value.style.outline = "none"
+
+    var valueHeight = ( options.valueHeight ) ? 
+                            options.valueHeight: "30px";
+    value.style.height = valueHeight;
+
+    var valueBorderRadius = ( options.valueBorderRadius ) ?
+                              options.valueBorderRadius: "5px";
+    value.style.borderRadius = valueBorderRadius;
+
     // now create the title
     var titleText = ( options.title ) ? options.title: "Slider";
     var title = screen.createChild( "div", null, null, titleText );
-    title.style.color = ( options.titleColor ) ? options.titleColor: "#000"
-    title.style.marginRight = "10%"
-    title.style.fontSize = ( options.titleFontSize ) ? options.titleFontSize: "30px"
+    var titleColor = ( options.titleColor ) ? options.titleColor: "#000"
+    title.style.color = titleColor;
+    var gapBetweenTitleAndValue = ( options.gapBetweenTitleAndValue ) ? 
+                                    options.gapBetweenTitleAndValue: "10%"
+    title.style.marginRight = gapBetweenTitleAndValue
+    var titleFontSize = ( options.titleFontSize ) ? 
+                          options.titleFontSize: "30px";
+    title.style.fontSize = titleFontSize
 
     // append it to the slider background
     sliderBackground.appendChild( container )
     container.appendChild( title )
     container.appendChild( value )
+    /**
+     * container of slider
+     */ 
+    var sliderContainer = screen.createChild( "div", null, null, "" );
+    sliderContainer.style.width = "95%";
+    sliderContainer.style.margin = "2% auto";
+    sliderContainer.style.display = "flex";
+    sliderContainer.style.alignItems = "center";
+    sliderContainer.style.justifyContent = "center";
+    sliderBackground.appendChild( sliderContainer )
+    /**
+     *  sides
+     */ 
+    var left = screen.createChild( "div", null, null, "" + sliderLow )
+    var right = screen.createChild( "div", null, null, "" + sliderTop )
+    var leftWidth = ( options.leftWidth ) ? options.leftWidth: "10%"
+    var rightWidth = ( options.rightWidth ) ? options.rightWidth: "10%"
+    var leftMargin = ( options.leftMargin ) ? options.leftMargin: "5%"
+    var rightMargin = ( options.rightMargin ) ? options.rightMargin: "5%"
+    left.style.marginRight = leftMargin;
+    right.style.marginLeft = rightMargin;
+    var leftFontSize = ( options.leftFontSize ) ? options.leftFontSize: "20px";
+    left.style.fontSize = leftFontSize;
+    var rigthFontSize = ( options.rigthFontSize ) 
+                          ? options.rigthFontSize: "20px";
+    right.style.fontSize = rigthFontSize;
 
-    // now get a slider
+    /**
+     * the actual slider
+     */ 
     var slider = screen.createChild( "div", null, null, "" )
-    slider.style.width = sliderBackground.style.width;
-    slider.style.border = ( options.sliderBorder ) ? options.sliderBorder: "1px solid black"
-    slider.style.borderRadius = ( options.sliderBorderRadius ) ? options.sliderBorderRadius: "10px" 
-    slider.style.height = ( options.sliderHeight ) ? options.sliderHeight: "2px"
-    slider.style.background = ( options.sliderBackground ) ? options.sliderBackground: "#555"
-    slider.style.position = "absolute";
-    // add the event listener
-    slider.onmouseover = function() {
-      slider.style.cursor = "pointer"
-    }
+    var sliderWidth = ( options.sliderWidth ) ? options.sliderWidth: "70%"
+    slider.style.width = sliderWidth;
+    slider.style.border = ( options.sliderBorder ) ? 
+                            options.sliderBorder: "1px solid black"
+    slider.style.borderRadius = ( options.sliderBorderRadius ) ? 
+                                  options.sliderBorderRadius: "10px" 
+    slider.style.height = ( options.sliderHeight ) ?
+                            options.sliderHeight: "2px"
+    slider.style.background = ( options.sliderBackground ) ? 
+                                options.sliderBackground: "#555"
 
-    // now add a slider thumb    
+    // rendering order ltr
+    sliderContainer.appendChild( left )
+    sliderContainer.appendChild( slider )
+    sliderContainer.appendChild( right )
+    /**
+     * the thumb of the slider
+     */     
     var thumb = screen.createChild( "div", null, null, "" );
-    thumb.style.position = "absolute";
-    let thumbWidth = ( options.thumbWidth ) ? options.thumbWidth: "8px";
+    let thumbWidth = ( options.thumbWidth ) ? options.thumbWidth: "10px";
     thumb.style.width = thumbWidth;
-    thumb.style.height = ( options.thumbHeight ) ? options.thumbHeight: "20px";
-    thumb.style.background = ( options.thumbWidth ) ? options.thumbBackground: "#222";
-    thumb.style.marginTop = ( options.thumbOffset ) ? options.thumbOffset: "-9px";
-    thumb.style.borderRadius = ( options.thumbBorderRadius ) ? options.thumbBorderRadius: "3px";
-    thumb.style.border = ( options.thumbBorder ) ? options.thumbBorder: "1px solid #444";
+    let thumbHeight = ( options.thumbHeight ) ? options.thumbHeight: "22px";
+    thumb.style.height = thumbHeight
+    // thumb.style.background = ( options.thumbWidth ) ?
+    //                            options.thumbBackground: "#4E2";
+    thumb.style.background = "none"
+    // halfway up
+    thumb.style.marginTop = ( options.thumbOffset ) ? options.thumbOffset: 
+                            "calc( -" + thumbHeight + " * 0.5"  + ")";
+    let thumbBorderRadius = ( options.thumbBorderRadius ) ? 
+                              options.thumbBorderRadius: "4px";
+    thumb.style.borderRadius = thumbBorderRadius;
+    thumb.style.border = ( options.thumbBorder ) ? 
+                    options.thumbBorder: "1px solid #00E";
+    /**
+     * shift the thumb based on startingValue
+     */ 
+    let percentage = ( parseFloat( startingValue ) - sliderLow) 
+                      / ( sliderTop - sliderLow ) * 100;
+    thumb.style.marginLeft = "calc( -" + thumbWidth + " * 0.5 + " +
+                           percentage + "% )";
 
     slider.appendChild( thumb )
 
-    sliderBackground.appendChild( slider )
     // event listeners
     slider.addEventListener("mousedown", function ( event ) { 
       let coord = ( event.pageX - slider.offsetLeft )
-      coord -= parseInt( thumbWidth.substring( 0, thumbWidth.length - 2 ) ) / 2
-      let length = slider.style.width
-      length = parseInt( length.substring( 0, length.length - 2 ) )
+      // coord -= parseInt( thumbWidth.substring( 0, thumbWidth.length - 2 ) ) / 2
+      let length = slider.clientWidth;
       if ( coord > length ) return;
       if ( coord < 0 ) return;
-      console.log( Math.round( coord / length * 100))
-      thumb.style.marginLeft = "" + coord + "px";
+      let percentage = coord / length * 100;
+      let newNumber = ( sliderTop - sliderLow ) * percentage / 100 + sliderLow;
+      //round the new Number 
+      newNumber = Math.round( newNumber * Math.pow( 10, round ) ) / 
+                            Math.pow( 10, round )
+      value.innerHTML = newNumber;
+      thumb.style.marginLeft = "calc( -" + thumbWidth + " * 0.5 + " +
+                           percentage + "% )";
     } );
+
+    // change the hover mouse
+    slider.onmouseover = function() {
+      slider.style.cursor = "pointer"
+    }
     return sliderBackground
 
   }
