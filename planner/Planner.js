860dcb4b5bc57cca3a9c2f4d70a79508a26e5803
Brandon Li <brandon.li820@icloud.com>
2019-02-01 23:00:28 -0700

    added top level file for planner

---
+++ planner/Planner.js
@@ -0,0 +1,17 @@
+/**
+ * Learning App
+ * Planner.js
+ *
+ * @author Brandon Li <brandon.li820@icloud.com> 
+ * Created on 1/31/19
+ * Copyright © 2019 Brandon Li. All rights reserved.
+ *
+ * top level file that creates the controller
+ *
+ */
+
+// import modules
+import PlannerController from "./Controllers/PlannerController.js";
+// modules
+var controller = new PlannerController( ); 
+controller.run();
