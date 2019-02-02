fcb83ff84d4c62d1d89839342a304aa5c992af7c
Brandon Li <brandon.li820@icloud.com>
2019-02-01 22:59:55 -0700

    changed the html to point to a file that runs the controller

--- planner/Controllers/PlannerController.js
+++ planner/Controllers/PlannerController.js
@@ -17,51 +17,13 @@
 'use strict';
 // import modules
 import PlannerView from "../Views/PlannerView.js";
-import PlannerModel from "../Models/PlannerModel.js"
+import PlannerModel from "../Models/PlannerModel.js";
+import PlannerNode from "../Models/PlannerNode.js";
+
 // modules
 // create the instances for the view and the model
 var view = new PlannerView( ); 
 var model = new PlannerModel( );
-/**
- * Intance Data
- * Set up the header and its children.
- */
-let header = view.addHeader( );
-
-let plannerIcon = view.addImageToHeader( );
-// add events
-plannerIcon.setAttribute( "src", "./assets/plannerTBackground.png" );
-// handle user input
-plannerIcon.onmouseover = function() { 
-  plannerIcon.setAttribute( "src", "./assets/plannerTBackgroundHover.png" );
-};
-plannerIcon.onmouseout = function() { 
-  plannerIcon.setAttribute( "src", "./assets/plannerTBackground.png" );
-};
-// back to dashboard button
-let backToDashboard = view.addLinkToHeader( "Back To Dashboard" );
-backToDashboard.onclick = function(){
-  window.open( "../dashboard.html", "_self" ) 
-}
-backToDashboard.style.border = "none";
-// add the help button
-let help = view.addLinkToHeader( "Help" )
-help.onclick = function(){
-  window.open( "./help.html", "_self" ) 
-}
-help.style.border = "none";
-// add the new plan button
-let newPlan = view.addLinkToHeader( "New" );
-newPlan.onclick = function(){
-  window.open( "/planner/new.html", "_self" ) 
-}
-newPlan.style.border = "none";
-// add the remove all
-let removeAll = view.addLinkToHeader( "Remove All" )
-removeAll.style.border = "none"
-removeAll.onclick = function( ){
-  model.clear();
-};
 
 /**
  * @public 
@@ -70,12 +32,73 @@ removeAll.onclick = function( ){
 export default class PlannerController{
   /**
    * @public 
+   * render the planner page
+   */
+  run( ){
+    this.renderBasics();
+    this.renderInOrder();
+  }
+  /**
+   * @public 
+   * render the header
+   */
+  renderBasics( ){
+    /**
+     * Intance Data
+     * Set up the header and its children.
+     */
+    let header = view.addHeader( );
+
+    let plannerIcon = view.addImageToHeader( );
+    // add events
+    plannerIcon.setAttribute( "src", "./assets/plannerTBackground.png" );
+    // handle user input
+    plannerIcon.onmouseover = function() { 
+      plannerIcon.setAttribute( "src", "./assets/plannerTBackgroundHover.png" );
+    };
+    plannerIcon.onmouseout = function() { 
+      plannerIcon.setAttribute( "src", "./assets/plannerTBackground.png" );
+    };
+    // back to dashboard button
+    let backToDashboard = view.addLinkToHeader( "Back To Dashboard" );
+    backToDashboard.onclick = function(){
+      window.open( "../dashboard.html", "_self" ) 
+    }
+    backToDashboard.style.border = "none";
+    // add the help button
+    let help = view.addLinkToHeader( "Help" )
+    help.onclick = function(){
+      window.open( "./help.html", "_self" ) 
+    }
+    help.style.border = "none";
+    // add the new plan button
+    let newPlan = view.addLinkToHeader( "New" );
+    newPlan.onclick = function(){
+      window.open( "/planner/new.html", "_self" ) 
+    }
+    newPlan.style.border = "none";
+    // add the remove all
+    let removeAll = view.addLinkToHeader( "Remove All" );
+    removeAll.style.border = "none"
+    removeAll.onclick = function( ){
+      model.clear();
+    };
+    this.header = header;
+    this.plannerIcon = plannerIcon;
+    this.help = help;
+    this.newPlan = newPlan;
+    this.removeAll = removeAll;
+  }
+  /**
+   * @public 
    * render everything from the model in order by popping elements from a heap.
    */
   renderInOrder(){
     let heap = model.getHeap( );
     let current = heap.pop();
-    console.log( current )
+    if( !current ){
+      view.displayNone();
+    }
     while ( current ){
       view.addCard( 
       current.title, parseInt( current.value ), 
@@ -83,13 +106,14 @@ export default class PlannerController{
       current = heap.pop();
     }
   }
+  /**
+   * @public 
+   * sumbit a new node on to the the model (doesn't refresh the screen)
+   */
+  submit( title, description, value, tags ){
+    model.add( new PlannerNode( value, title, description, tags ) );
+  }
+
 }
-
-let controller = new PlannerController();
-controller.renderInOrder();
-
-
-
-
 
 
