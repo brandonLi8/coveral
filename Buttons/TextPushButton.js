72277aa1285139bb7e69e9a7078d8cf472897618
Brandon Li <brandon.li820@icloud.com>
2019-02-14 14:04:01 -0700

    update button module

--- Buttons/TextPushButton.js
+++ Buttons/TextPushButton.js
@@ -9,13 +9,16 @@
  * Supported for DOM, inheritance of Node
  *
  * ## Description:
- * A Node that takes a text, its hover color, and its normal color
- * that is a button. The user will provide the styling and the listener function
- * when the button is pressed
+ * A Node that has all the nodes for a button ( text ).
+ * This provides a default looking button, but the user can overide the style.
+ * The style is a seperate opbject that the user can default each item, so
+ * if you override style it doens't delete all of the style.
  *
+ *
+ * This class will provide functions that animate the button when called
  */
 
-
+// modules
 import Node from "../Screen/Node.js";
 
 "use strict";
@@ -27,54 +30,83 @@ export default class TextPushButton {
    * @constructor
    *
    * @param {object} options - look at defaults
-   * @return {Node} the node containing all the attributes of a button
+   * ( to get the button node call the .button property of this class )
    */
   constructor( options ){
-    // provide the defaults
+    // provide the defaults @overidable
     const defaults = {
-      text: null,
-      style: null,
-      hoverStyle: null,
+      // @optional the text on the node <string>
+      text: "Button", 
+
+      // @optional the styling ( overriding doesnt delete all of it )
+      style: { 
+        border: "1px solid #222",
+        borderRadius: "15px",
+        width: "200px",
+        height: "50px",
+        display: "flex",
+        background: "white",
+        boxShadow: "0 0 3px 0 rgb( 40, 40, 40 )",
+        margin: "auto"
+      },
+
+      // @optional the styling on the hover
+      hoverStyle: { 
+        cursor: "pointer",
+        background: "#DDD"
+      },
+
+      // @optional the style for the text ( mostly used for )
+      textStyle: { 
+        fontSize: "20px",
+        margin: "auto",
+        textAlign: "center",
+        fontFamily: "Courier"
+      },
+
+      // @optional the style for the text on the button hover
+      textHoverSyle: null,
+      // the id of the button <string>
       id: null, 
-      class: null,
-      listener: null
+      // the class of the button <string>
+      class: null, 
+      // the function called on the click <function> (caller's scope)
+      onclick: null,
+      // the function called on the hover <function> (caller's scope)
+      onhover: null
+
     }
     // merge them with options overriding
-    const attributes = { ...defaults, ...options }; 
-    if ( attributes.style ){
-      this.button = new Node({
-        style: attributes.style
-      });
-    }
-    
+    const attributes = { ...defaults, ...options };
+    // merge the styles 
+    attributes.style = { ...defaults.style, ...options.style }
+    attributes.hoverStyle = { ...defaults.hoverStyle, ...options.hoverStyle } 
+    attributes.textStyle = { ...defaults.textStyle, ...options.hoverStyle }
+    // we know the style has to exist, set the style for the button
+    // @very public ( the purpose of this class )
     this.button = new Node({
       style: attributes.style
     });
-    // hover effect
-    if ( attributes.hoverStyle ){
-      this.button.addEventListener( "mouseover", _ => 
-        hover( this.button ) 
-      );
-      this.button.addEventListener( "mouseout", _ => 
-        unhover( this.button )
-      );
-    }
-    function hover( node ){
-      // no matter what, change the cursor
-      node.setStyle({
-        cursor: "pointer",
-        ...attributes.hoverStyle,
-      })
-    }
-    function unhover( node ){
-      node.setStyle( attributes.style )
-    }
-    
-    if ( attributes.listener ){
-      this.button.addEventListener( "mousedown", function(){
-        attributes.listener();
-      } );
-    }
-    return this.button;
+    // add hover styling ( you can call animations from your css in here )
+    this.button.addEventListener( "mouseover", _ => 
+      this.button.setStyle( attributes.hoverStyle )
+    );
+    this.button.addEventListener( "mouseout", _ => 
+      this.button.setStyle( attributes.style )
+    );
+    // on click listener
+    this.button.addEventListener( "mousedown", function(){
+      if ( attributes.listener ) attributes.listener();
+    } );
+    /**
+     * The reason for a seperate node for text is that the user can provide
+     * more animations like wiggling the text on a hover, etc.
+     */
+    this.textNode = new Node({
+      text: attributes.text,
+      style: attributes.textStyle
+    })
+    this.button.addChild( this.textNode )
+    return this.button
   }
 }
