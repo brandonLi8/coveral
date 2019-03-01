e1048c000bb8332a690f70eeb1eb8842eeba679a
Brandon Li <brandon.li820@icloud.com>
2019-03-01 11:38:30 -0700

    added Circular Motion

---
+++ CircularMotion/Js/Models/CircularMotionModel.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Learning App
+ * CircularMotionModel.js
+ *
+ * @author Brandon Li <brandon.li820@icloud.com> 
+ * Created on 3/1/19
+ * Copyright © 2019 Brandon Li. All rights reserved.
+ *
+ * Model for Circular Motion
+ * 
+ */
